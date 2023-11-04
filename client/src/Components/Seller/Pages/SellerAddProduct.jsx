6b6a8324a98160f6d0ec6afb97195c08ece62c57
thdihan <thdihan@outlook.com>
2023-11-05 00:21:06 +0600

    UI Test running

--- client/src/Components/Seller/Pages/SellerAddProduct.jsx
+++ client/src/Components/Seller/Pages/SellerAddProduct.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import classes from "../../../Style/Seller/SellerAddCategory.module.css";
 
 export default function SellerAddProduct() {
+    const [selectedFiles, setSelectedFiles] = useState([]);
+
+    const handleFileChange = (e) => {
+        // Get the selected files from the input element
+        const files = e.target.files;
+        const selectedFilesArray = [];
+
+        for (let i = 0; i < files.length; i++) {
+            selectedFilesArray.push(files[i]);
+        }
+
+        // Update the state with the selected files
+        setSelectedFiles(selectedFilesArray);
+    };
     function handleSubmit(e) {
         e.preventDefault();
+        console.log(selectedFiles);
         const formData = new FormData(e.target);
         const formDataObject = Object.fromEntries(formData);
         console.log("Form Data Example : ", formDataObject);
@@ -15,121 +31,26 @@ export default function SellerAddProduct() {
             <div className="p-3">
                 <form
                     onSubmit={handleSubmit}
+                    encType="multipart/form-data"
                     className={`row gx-3 ${classes["add-category-form"]}`}
                 >
-                    <div className=" col col-md-12">
-                        <label htmlFor="category" className="form-label">
-                            Product Name
-                        </label>
+                    <div className="col-12">
+                        <label htmlFor="product-name">Product Name</label>
                         <input
+                            type="text"
                             name="product-name"
-                            type="text"
-                            className="form-control p-2"
-                            id="category"
+                            id="product-name"
                         />
                     </div>
-                    <div className="col col-md-6">
-                        <label htmlFor="base-price" className="form-label">
-                            Base Price
-                        </label>
+                    <div className="col-12">
+                        <label htmlFor="product-image">Product Image</label>
                         <input
-                            name="base-price"
-                            type="text"
-                            className="form-control p-2"
-                            id="base-price"
-                        />
-                    </div>
-                    <div className="col col-md-6">
-                        <label htmlFor="base-price" className="form-label">
-                            Discount
-                        </label>
-                        <input
-                            name="discount"
-                            type="text"
-                            className="form-control p-2"
-                            id="discount"
-                        />
-                    </div>
-
-                    <div className="col col-md-12">
-                        <label htmlFor="base-price" className="form-label">
-                            Stock
-                        </label>
-                        <input
-                            name="stock"
-                            type="text"
-                            className="form-control p-2"
-                            id="stock"
-                        />
-                    </div>
-                    <div className="col col-md-12">
-                        <label htmlFor="floatingTextarea2">
-                            Product Description
-                        </label>
-                        <textarea
-                            className="form-control"
-                            name="description"
-                            id="floatingTextarea2"
-                            style={{ height: "100px" }}
-                        ></textarea>
-                    </div>
-                    <div className="col col-md-12">
-                        <label htmlFor="formFile" className="form-label">
-                            Product Image
-                        </label>
-                        <input
-                            className="form-control"
+                            multiple
                             type="file"
-                            id="formFile"
+                            name="product-image"
+                            id="product-image"
+                            onChange={handleFileChange}
                         />
-                    </div>
-
-                    <div
-                        className={`dropdown ${classes["parent-category"]} col-md-12`}
-                    >
-                        <button
-                            className="btn btn-secondary dropdown-toggle p-2 mt-3"
-                            type="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            Parent Category
-                        </button>
-                        <ul className="dropdown-menu">
-                            <li className="p-2">
-                                <label htmlFor="" className="d-inline">
-                                    <input
-                                        type="checkbox"
-                                        name="cat1"
-                                        value="cat1"
-                                        id=""
-                                    />{" "}
-                                    Category
-                                </label>
-                            </li>
-                            <li className="p-2">
-                                <label htmlFor="" className="d-inline">
-                                    <input
-                                        type="checkbox"
-                                        name="cat2"
-                                        value="cat2"
-                                        id=""
-                                    />{" "}
-                                    Category
-                                </label>
-                            </li>
-                            <li className="p-2">
-                                <label htmlFor="" className="d-inline">
-                                    <input
-                                        type="checkbox"
-                                        name="cat3"
-                                        value="cat3"
-                                        id=""
-                                    />{" "}
-                                    Category
-                                </label>
-                            </li>
-                        </ul>
                     </div>
                     <input
                         className={`btn py-2 mt-2 ${classes["add-category-btn"]}`}
@@ -137,6 +58,20 @@ export default function SellerAddProduct() {
                         value="Add Product"
                     />
                 </form>
+
+                <div>
+                    <h2>Selected Image Files:</h2>
+                    {selectedFiles.map((file, index) => (
+                        <div key={index}>
+                            <p>{file.name}</p>
+                            <img
+                                src={URL.createObjectURL(file)}
+                                alt={file.name}
+                                style={{ maxWidth: "200px" }}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
