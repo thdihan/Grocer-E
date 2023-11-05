4e06fd761c6da79f483a9eda64d3756dc5fa16a0
thdihan <thdihan@outlook.com>
2023-11-05 11:32:19 +0600

    category field sorted

--- client/src/Components/Seller/Pages/SellerAddProduct.jsx
+++ client/src/Components/Seller/Pages/SellerAddProduct.jsx
@@ -1,9 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "../../../Style/Seller/SellerAddCategory.module.css";
+import TextInput from "../../Common/FormComponents/TextInput";
 
 export default function SellerAddProduct() {
     const [selectedFiles, setSelectedFiles] = useState([]);
+    const [modifiedCategoryList, setModifiedCategoryList] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState([]);
 
+    useEffect(() => {
+        console.log("Run Use Effect");
+        setSelectedCategory(modifiedCategoryList);
+    }, [modifiedCategoryList]);
+    const addCategorySelection = (e) => {
+        // console.log("Modified Category ");
+        // const getSelection = modifiedCategoryList;
+        // if (!getSelection.includes(e.target.value)) {
+        //     console.log("Adding Category: ", e.target.value);
+        //     getSelection.push(e.target.value);
+        // }
+
+        // console.log(getSelection);
+        // setModifiedCategoryList(() => getSelection);
+        const category = e.target.value;
+        setSelectedCategory((prevSelected) => [...prevSelected, category]);
+    };
+
+    const deleteCategorySelection = (e) => {
+        // console.log("Modified Category ", modifiedCategoryList);
+        // const getSelection = modifiedCategoryList;
+
+        // const index = getSelection.findIndex(
+        //     (element) => element === e.target.value
+        // );
+        // getSelection.splice(index, 1);
+
+        // console.log(getSelection);
+        // setModifiedCategoryList(() => getSelection);
+
+        const category = e.target.value;
+        setSelectedCategory((prevSelected) =>
+            prevSelected.filter((item) => item !== category)
+        );
+    };
     const handleFileChange = (e) => {
         // Get the selected files from the input element
         const files = e.target.files;
@@ -34,14 +72,128 @@ export default function SellerAddProduct() {
                     encType="multipart/form-data"
                     className={`row gx-3 ${classes["add-category-form"]}`}
                 >
-                    <div className="col-12">
-                        <label htmlFor="product-name">Product Name</label>
-                        <input
-                            type="text"
-                            name="product-name"
-                            id="product-name"
-                        />
+                    <TextInput
+                        bclass={`col-12 my-2`}
+                        id={`product-name`}
+                        type={`text`}
+                        name="product-name"
+                        labelText={`Product Name`}
+                    />
+
+                    <div className="col-12 my-2">
+                        <div className="row gx-2">
+                            <TextInput
+                                bclass={`col col-6 pe-2`}
+                                id={`base-price`}
+                                type={`text`}
+                                name="base-price"
+                                labelText={`Base Price (tk)`}
+                            />
+                            <TextInput
+                                bclass={`col col-6 ps-2`}
+                                id={`discount`}
+                                type={`text`}
+                                name="discount"
+                                labelText={`Discount (%)`}
+                            />
+                        </div>
                     </div>
+                    <div className="col-12 my-2">
+                        <div className="row gx-2">
+                            <TextInput
+                                bclass={`col col-6 pe-2`}
+                                id={`stock`}
+                                type={`text`}
+                                name="stock"
+                                labelText={`Avaiable Stock`}
+                            />
+
+                            <TextInput
+                                bclass={`col col-6 ps-2`}
+                                id={`unit`}
+                                type={`text`}
+                                name="unit"
+                                labelText={`Unit of product`}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="col-12 my-2">
+                        <div className="container-fluid">
+                            <div
+                                className={`${classes["category-header"]} p-2 row`}
+                            >
+                                <div
+                                    className={`col-10 d-flex align-items-center ${classes["selected-category-list"]}`}
+                                >
+                                    <p>Selected Category: </p>{" "}
+                                    <div
+                                        className={`${classes["selected-category"]} d-flex ms-2`}
+                                    >
+                                        {selectedCategory?.map((category) => (
+                                            <p
+                                                key={category}
+                                                className="d-flex align-items-center rounded-pill px-2 me-2"
+                                            >
+                                                <span className="me-2">
+                                                    {category}
+                                                </span>
+                                            </p>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div
+                                    className={`col-2 ${classes["category-search-box"]}`}
+                                >
+                                    <input
+                                        type="text"
+                                        className={`py-2`}
+                                        name=""
+                                        id=""
+                                    />
+                                </div>
+                            </div>
+                            <div
+                                className={`${classes["category-details"]} p-2 row`}
+                            >
+                                <div
+                                    className={`col-12 d-flex flex-wrap ${classes["category-list"]}`}
+                                >
+                                    <label htmlFor="checkbox1" className="me-3">
+                                        <input
+                                            type="checkbox"
+                                            className="me-2"
+                                            value={`category1`}
+                                            onChange={(e) => {
+                                                e.target.checked
+                                                    ? addCategorySelection(e)
+                                                    : deleteCategorySelection(
+                                                          e
+                                                      );
+                                            }}
+                                        />
+                                        Category 1
+                                    </label>
+                                    <label htmlFor="checkbox2" className="me-3">
+                                        <input
+                                            type="checkbox"
+                                            className="me-2"
+                                            value={`category2`}
+                                            onChange={(e) => {
+                                                e.target.checked
+                                                    ? addCategorySelection(e)
+                                                    : deleteCategorySelection(
+                                                          e
+                                                      );
+                                            }}
+                                        />
+                                        Category 2
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className="col-12">
                         <label htmlFor="product-image">Product Image</label>
                         <input
