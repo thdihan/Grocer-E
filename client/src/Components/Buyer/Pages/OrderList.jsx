22ed8ba5c9ea52b34ae258040a38cb792eb4f37b
thdihan <thdihan@outlook.com>
2023-12-12 00:10:50 +0600

    Order List done

--- client/src/Components/Buyer/Pages/OrderList.jsx
+++ client/src/Components/Buyer/Pages/OrderList.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import classes from "../../../Style/Buyer/OrderList.module.css";
 
 const OrderList = () => {
@@ -9,6 +10,62 @@ const OrderList = () => {
                 <h5 className={`m-0`}>
                     <i className="fa-solid fa-table-list pe-2"></i> Order List
                 </h5>
+            </div>
+
+            <div className={`px-4 py-3 table-responsive`}>
+                <table className={`table w-100`}>
+                    <thead>
+                        <tr>
+                            <th>Order ID</th>
+                            <th>Order Date</th>
+                            <th>Order Total</th>
+                            <th>Order Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>#2025422</td>
+                            <td>12-12-2023</td>
+                            <td>120 tk</td>
+                            <td className={`fw-semibold`}>Pending</td>
+                            <td>
+                                <Link className={`btn`}>View Details</Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>#2025422</td>
+                            <td>12-12-2023</td>
+                            <td>120 tk</td>
+                            <td className={`text-warning fw-semibold`}>
+                                Approved
+                            </td>
+                            <td>
+                                <Link className={`btn`}>View Details</Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>#2025422</td>
+                            <td>12-12-2023</td>
+                            <td>120 tk</td>
+                            <td className={`text-info fw-semibold`}>Shipped</td>
+                            <td>
+                                <Link className={`btn`}>View Details</Link>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>#2025422</td>
+                            <td>12-12-2023</td>
+                            <td>120 tk</td>
+                            <td className={`text-success fw-semibold`}>
+                                Completed
+                            </td>
+                            <td>
+                                <Link className={`btn`}>View Details</Link>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     );
