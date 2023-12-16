194079b4e7fe3c3557008076661b3a0ec92b7907
thdihan <thdihan@outlook.com>
2023-12-16 13:26:10 +0600

    Order Status Change

--- client/src/Components/Seller/Pages/SellerSingleOrder.jsx
+++ client/src/Components/Seller/Pages/SellerSingleOrder.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import classes from "../../../Style/Seller/SellerSingleOrder.module.css";
-
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
 const SellerSingleOrder = () => {
+    const [orderStatus, setOrderStatus] = useState("Pending");
+
+    const updateStatus = (e) => {
+        // Close the modal here
+        const modal = document.getElementById("exampleModal");
+        const modalInstance = bootstrap.Modal.getInstance(modal);
+        modalInstance.hide();
+
+        // Clear the modal backdrop overlay
+        const modalBackdrop = document.querySelector(".modal-backdrop");
+        if (modalBackdrop) {
+            modalBackdrop.remove();
+        }
+
+        // [Todo] : Update the order status here
+    };
     return (
         <div className={`${classes["SellerSingleOrder"]}`}>
             <div className="px-3 py-3 border-bottom d-flex justify-content-between align-items-center">
@@ -44,7 +61,17 @@ const SellerSingleOrder = () => {
                     </tr>
                     <tr>
                         <td>Order Status</td>
-                        <td>Pending...</td>
+                        <td className="">
+                            {orderStatus}
+                            <button
+                                type="button"
+                                className="btn ms-5"
+                                data-bs-toggle="modal"
+                                data-bs-target="#exampleModal"
+                            >
+                                Change Status
+                            </button>
+                        </td>
                     </tr>
                     <tr>
                         <td>Total Price</td>
@@ -86,6 +113,68 @@ const SellerSingleOrder = () => {
                         <td>1200 tk</td>
                     </tr>
                 </table>
+            </div>
+
+            <div
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1
+                                className="modal-title fs-5"
+                                id="exampleModalLabel"
+                            >
+                                Modal title
+                            </h1>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <select
+                                    name="status"
+                                    id=""
+                                    value={orderStatus}
+                                    onChange={(e) =>
+                                        setOrderStatus(e.target.value)
+                                    }
+                                >
+                                    <option value="Pending">Pending</option>
+                                    <option value="Processing">
+                                        Processing
+                                    </option>
+                                    <option value="Delivered">Delivered</option>
+                                    <option value="Cancelled">Cancelled</option>
+                                </select>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button
+                                onClick={updateStatus}
+                                type="submit"
+                                className="btn btn-primary"
+                            >
+                                Update Status
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
