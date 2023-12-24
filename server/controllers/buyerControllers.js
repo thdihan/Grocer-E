06405f564a3a3e27578ae1eaf11a07bb2efdf906
cabbage <shakun650@gmail.com>
2023-12-24 18:30:52 +0600

    Product sell quantity added

--- server/controllers/buyerControllers.js
+++ server/controllers/buyerControllers.js
@@ -6,7 +6,7 @@ const getProducts = async (req, res) => {
   try {
     if (category === undefined) {
       const products = await pool.query(
-        "SELECT p.product_id, p.product_name, p.description, p.base_price, p.discount, p.unit, p.stock, p.product_image, array_agg(c.category_id) AS category_ids, array_agg(c.category_name) AS category_names FROM products AS p INNER JOIN product_category_relationship AS pcr ON p.product_id = pcr.product_id INNER JOIN categories AS c ON pcr.category_id = c.category_id GROUP BY p.product_id, p.product_name, p.description, p.base_price, p.discount, p.unit, p.stock, p.product_image;"
+        "SELECT p.product_id, p.product_name, p.description, p.base_price, p.discount, p.unit, p.stock, p.product_image, array_agg(c.category_id) AS category_ids, array_agg(c.category_name) AS category_names, COALESCE(SUM(op.quantity), 0) AS sell_quantity FROM products AS p INNER JOIN product_category_relationship AS pcr ON p.product_id = pcr.product_id INNER JOIN categories AS c ON pcr.category_id = c.category_id LEFT JOIN ordered_product AS op ON p.product_id = op.product_id GROUP BY p.product_id, p.product_name, p.description, p.base_price, p.discount, p.unit, p.stock, p.product_image;"
       );
       console.log(products);
       res.status(200).json({
@@ -14,7 +14,7 @@ const getProducts = async (req, res) => {
       });
     } else {
       const products = await pool.query(
-        "SELECT p.product_id, p.product_name, p.description, p.base_price, p.discount, p.unit, p.stock, p.product_image,( SELECT array_agg(c.category_id)  FROM product_category_relationship AS pcr INNER JOIN categories AS c ON pcr.category_id = c.category_id WHERE pcr.product_id = p.product_id ) AS category_ids, ( SELECT array_agg(c.category_name) FROM product_category_relationship AS pcr INNER JOIN categories AS c ON pcr.category_id = c.category_id WHERE pcr.product_id = p.product_id ) AS category_names FROM products AS p WHERE EXISTS ( SELECT 1 FROM product_category_relationship AS pcr WHERE pcr.product_id = p.product_id AND pcr.category_id = $1);",
+        "SELECT p.product_id, p.product_name, p.description, p.base_price, p.discount, p.unit, p.stock, p.product_image, (SELECT array_agg(c.category_id) FROM product_category_relationship AS pcr INNER JOIN categories AS c ON pcr.category_id = c.category_id WHERE pcr.product_id = p.product_id) AS category_ids, (SELECT array_agg(c.category_name) FROM product_category_relationship AS pcr INNER JOIN categories AS c ON pcr.category_id = c.category_id WHERE pcr.product_id = p.product_id) AS category_names, COALESCE((SELECT SUM(op.quantity) FROM ordered_product AS op WHERE op.product_id = p.product_id), 0) AS sell_quantity FROM products AS p WHERE EXISTS ( SELECT 1 FROM product_category_relationship AS pcr WHERE pcr.product_id = p.product_id AND pcr.category_id = $1 );",
         [category]
       );
       console.log(products);
@@ -122,3 +122,5 @@ module.exports = {
   getFrequentBoughtProducts,
   getLastOrderedDate,
 };
+
+("SELECT p.product_id, p.product_name, p.description, p.base_price, p.discount, p.unit, p.stock, p.product_image, (SELECT array_agg(c.category_id) FROM product_category_relationship AS pcr INNER JOIN categories AS c ON pcr.category_id = c.category_id WHERE pcr.product_id = p.product_id) AS category_ids, (SELECT array_agg(c.category_name) FROM product_category_relationship AS pcr INNER JOIN categories AS c ON pcr.category_id = c.category_id WHERE pcr.product_id = p.product_id) AS category_names, COALESCE((SELECT SUM(op.quantity) FROM ordered_product AS op WHERE op.product_id = p.product_id), 0) AS sell_quantity FROM products AS p WHERE EXISTS ( SELECT 1 FROM product_category_relationship AS pcr WHERE pcr.product_id = p.product_id AND pcr.category_id = $1 );");
